d421e94d7a06f7d1dcf289893cd78d6a39eb5583
Med AMRI <amri.med.a@gmail.com>
2023-07-20 06:19:52 -0400

    Ajout des commentaires

--- Tache2_Calcule_Score.sql
+++ Tache2_Calcule_Score.sql
@@ -1,5 +1,10 @@
 -- Créer la vue "score"
 
+
+-- On calcule le max_length de toute la colonne une seule fois en utilisant une requête 
+-- de sous-requête CTE (Common Table Expression). De cette façon, nous n'avons pas besoin 
+-- de recalculer le max_length pour chaque ligne de la table lors de l'exécution de la vue.
+
 CREATE OR REPLACE VIEW "mohamed-abdelkayoum.amri.1@ens.etsmtl.ca".score AS
 WITH max_length_cte AS (
     SELECT ROUND(MAX(LENGTH(html_content)), 1) AS max_length
