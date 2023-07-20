998c66be9494d7394ae0c132ea3459bd525d832c
Med AMRI <amri.med.a@gmail.com>
2023-07-20 06:17:32 -0400

    calcule le max_length de toute la colonne une seule fois

--- Tache2_Calcule_Score.sql
+++ Tache2_Calcule_Score.sql
@@ -1,6 +1,11 @@
 -- Créer la vue "score"
 
 CREATE OR REPLACE VIEW "mohamed-abdelkayoum.amri.1@ens.etsmtl.ca".score AS
+WITH max_length_cte AS (
+    SELECT ROUND(MAX(LENGTH(html_content)), 1) AS max_length
+    FROM louis_v004.crawl
+)
 SELECT *,
-       ROUND(LENGTH(html_content) / (SELECT round(MAX(LENGTH(html_content)),1) FROM louis_v004.crawl), 1) AS score
-FROM louis_v004.crawl;
+       ROUND(LENGTH(html_content) / max_length, 1) AS score
+FROM louis_v004.crawl
+CROSS JOIN max_length_cte;
